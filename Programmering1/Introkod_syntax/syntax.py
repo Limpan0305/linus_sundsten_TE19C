7639caa28eb716d3ae85553305a20caa3c5c501d
Limpan0305 <70264385+Limpan0305@users.noreply.github.com>
2020-09-14 13:42:12 +0200

    första commit med många filer och bra struktur

---
+++ Programmering1/Introkod_syntax/syntax.py
@@ -0,0 +1,13 @@
+print("Hej TE19C")
+
+# skapar en variabel och tilldelar det värdet "Linus"
+name = "Linus"
+
+#print (name)
+print(f"Ditt namn är {name}")
+
+# float typomvandlar strängen till ett flyttal för att kunna räkna med det
+side = float(input("Ange kvadratens sida: "))
+area = side**2
+
+print(f"Arean av kvadraten är {area} a.e.")
